1c71f0bab21548595c39600edd4447cfa4a3588d
MrRisoni <vanguelisk@gmail.com>
2020-12-22 13:22:30 +0200

    mysql

--- daos/products_dao.js
+++ daos/products_dao.js
@@ -1,17 +1,28 @@
 const knexConfig = require('./../knex_config');
 
 function  getBoughtWith() {
+
+    console.log(knexConfig
+        .select('oi2.PRODUCT_ID', 'p.TITLE','p.THUMBNAIL')
+        .from('ORDER_ITEMS AS oi2')
+        .join('PRODUCTS AS p', 'p.ID', 'oi2.PRODUCT_ID')
+        .join('ORDER_ITEMS AS oi1', 'oi1.ORDER_ID', 'oi2.ORDER_ID')
+        .select('oi2.PRODUCT_ID', 'p.TITLE')
+        .where('oi1.PRODUCT_ID', '=', 1)
+        .where('oi2.PRODUCT_ID', '!=', 1)
+        .groupBy('oi2.PRODUCT_ID')
+        .orderByRaw(' COUNT(oi2.ID) DESC').toSQL().toNative());
+
     return knexConfig
-    .select('oi2.PRODUCT_ID', 'p.TITLE','p.THUMBNAIL')
-    .from('ORDER_ITEMS AS oi2')
-    .join('PRODUCTS AS p', 'p.ID', 'oi2.PRODUCT_ID')
-    .join('ORDER_ITEMS AS oi1', 'oi1.ORDER_ID', 'oi2.ORDER_ID')
-    .select('oi2.PRODUCT_ID', 'p.TITLE')
-    .where('oi1.PRODUCT_ID', '=', 1)
-    .where('oi2.PRODUCT_ID', '!=', 1)
-    .whereRaw(' ROWNUM < 3 ')
-    .groupBy('oi2.PRODUCT_ID','p.TITLE','p.THUMBNAIL')
-    .orderByRaw(' COUNT("oi2"."ID") DESC');
+    .select('oi2.product_id', 'p.title','p.thumbnail_url')
+    .from('order_items AS oi2')
+    .join('products AS p', 'p.id', 'oi2.product_id')
+    .join('order_items AS oi1', 'oi1.order_id', 'oi2.order_id')
+    .select('oi2.product_id', 'p.TITLE')
+    .where('oi1.product_id', '=', 1)
+    .where('oi2.product_id', '!=', 1)
+    .groupBy('oi2.product_id')
+    .orderByRaw(' COUNT(oi2.id) DESC');
 }
 
 module.exports = {getBoughtWith}
--- knex_config.js
+++ knex_config.js
@@ -20,4 +20,20 @@ var knex = require('knex')({
   }
 });
 
+var MYSQL_NAME = process.env.SPRING_APP_DB_ECOMMERCE_USR;
+var MYSQL_PASSWD = process.env.SPRING_APP_DB_ECOMMERCE_PASSWD;
+var MYSQL_HOST = process.env.SPRING_APP_DB_ECOMMERCE_HOST;
+var MYSQL_DB= process.env.SPRING_APP_DB_ECOMMERCE_NAME;
+
+knex = require('knex')({
+  client: 'mysql',
+  connection: {
+    host : MYSQL_HOST,
+    user : MYSQL_NAME,
+    password : MYSQL_PASSWD,
+    database : MYSQL_DB
+  }
+});
+
+
 module.exports = knex
